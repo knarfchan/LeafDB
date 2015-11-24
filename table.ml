6b2e1c979ba1f517894eb973627882f4ada1f2fa
Erin <ec587@cornell.edu>
2015-11-23 22:27:56 -0500

    Created table.ml and btree.ml. Variant btree types in progress.

---
+++ table.ml
@@ -0,0 +1,13 @@
+(* table.ml *)
+open Query
+open Btree
+
+type 'a node = {mutable prev: 'a node option; mutable next: 'a node option;
+                value: 'a}
+
+type 'a dlist = {mutable first: 'a node option; mutable last: 'a node option;}
+
+type t = 'a dlist
+
+(* a match between columns for relating tables *)
+type on = column * column
