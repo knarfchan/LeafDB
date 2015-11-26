8411c5d71a27ae0aa8de5eed90093d5cc95d231b
Erin <ec587@cornell.edu>
2015-11-26 00:12:52 -0500

    Creation of maps.ml

---
+++ maps.ml
@@ -0,0 +1,37 @@
+(*maps.ml*)
+open Query
+open Date
+
+let compare_dates d1 d2 = 0
+
+module Int = struct
+  type t = int
+  let compare = Pervasives.compare
+end
+
+module String = struct
+  type t = string
+  let compare = Pervasives.compare
+end
+
+module Bool = struct
+  type t = bool
+  let compare = Pervasives.compare
+end
+
+module Float = struct
+  type t = float
+  let compare = Pervasives.compare
+end
+
+module Dates = struct
+  type t = Date.t
+  let compare = compare_dates
+end
+
+let type_map (v: Query.value) = match v with
+  | VInt _    -> Map.Make (Int)
+  | VString _ -> Map.Make (String)
+  | VBool _   -> Map.Make (Bool)
+  | VFloat _  -> Map.Make (Float)
+  | VDate _   -> Map.Make (Dates)
