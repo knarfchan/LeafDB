619c1d00239e0924c2edc129dc8c6eaa4db6856f
louis <cruzlouis13@gmail.com>
2015-11-28 00:49:00 -0500

    add select for map

--- maps.ml
+++ maps.ml
@@ -1,5 +1,4 @@
 (*maps.ml*)
-open Query
 
 module Int = struct
   type t = int
@@ -21,28 +20,63 @@ module Float = struct
   let compare = Pervasives.compare
 end
 
-type date = int * int * int (* (year, month, day) *)
-
 module Date : Map.OrderedType = struct
-  type t = date
-  let compare = Pervasives.compare
+  type t = Core.Date.t
+  let compare = Core.Date.compare
 end
 
 module IntMap    = Map.Make (Int)
 module StringMap = Map.Make (String)
 module BoolMap   = Map.Make (Bool)
 module FloatMap  = Map.Make (Float)
-module DateMap   = Map.Make(Date)
+module DateMap   = Map.Make (Date)
 
-let lookup = failwith "Unimplemeted"
+let lookup map k = failwith "Unimplemeted"
 
-let select (w: where) = failwith "Unimplemented"
+let like_compare value element typ =
+  open Str in
+  match typ with
+  | LikeBegin -> string_match (regex (".*"^element) value)
+  | LikeEnd -> string_match (regex (element^".*") value)
+  | LikeSubstring -> string_match (regex (".*"^element^".*") value)
+  | NotLikeBegin -> not (string_match (regex (".*"^element) value))
+  | NotLikeEnd -> not (string_match (regex (element^".*") value))
+  | NotLikeSubstring -> not (string_match (regex (".*"^element^".*") value))
+
+let compare element value typ =
+  match typ with
+  | Date.t -> Date.compare element value
+  | _ -> Pervasives.compare element value
+
+let does_satisfy condition value element typ =
+  let var = compare element value typ in
+  match condtion with
+  | Gt -> var > 0
+  | Lt -> var < 0
+  | Eq -> var = 0
+  | GtEq -> var > 0 || var = 0
+  | LtEq -> var < 0 || var = 0
+  | NotEq -> var <> 0
+  | _ -> like_compare value element typ
+
+let get = function
+  | VInt i -> i
+  | VString s -> s
+  | VBool b -> b
+  | VFloat f -> f
+  | VDate d -> d
+
+let select map condition value =
+  match value with
+  | VInt i -> IntMap.filter (fun key e -> does_satisfy condition i (get e) map.t) map
+  | VString s -> StringMap.filter (fun key e -> does_satisfy condition s (get e) map.t) map
+  | VBool b -> BoolMap.filter (fun key e -> does_satisfy condition b (get e) map.t) map
+  | VFloat f -> FloatMap.filter (fun key e -> does_satisfy condition f (get e) map.t) map
+  | VDate d -> DateMap.filter (fun key e -> does_satisfy condition d (get e) map.t) map
 
 let insert (lst: value list) = failwith "Unimplemented"
 
 let update (w: where) (v: value) = failwith "Unimplemented"
-
-let updateAll (lst: value list) = failwith "Unimplemented"
 
 let delete (lst: value list) = failwith "Unimplemented"
 
