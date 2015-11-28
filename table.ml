ba414fcff396da35b49680e8f9951585ae41e9b9
Frank Chan <fc249@cornell.edu>
2015-11-28 00:14:33 -0500

    separate table and types

--- table.ml
+++ table.ml
@@ -1,45 +1,12 @@
 (* table.ml *)
-open Core.Date
-
-(* represent our table columns as a string *)
-type column  = string
-
-(* a match between columns for relating tables *)
-type on = column * column
-
-(*(year, month, day)*)
-type date = int * int * int
-
-(* data types our database support *)
-type value =
-  | VInt of int
-  | VString of string
-  | VBool of bool
-  | VFloat of float
-  | VDate of int
-
-(* supported operators in where used to conditionally select rows *)
-type operator =
-  | Gt | Lt | Eq | GtEq | LtEq | NotEq
-  | LikeBegin | LikeEnd | LikeSubstring
-  | NotLikeBegin | NotLikeEnd | NotLikeSubstring
-
-(* SQL where expression: operate on the columns with the given value if there
- * is a condition, or Null if there is no condition
- *)
-type where    =
-  | Condition of (column * operator * value)
-  | Null
-
-(* declaration of a column with its associated value *)
-type column_dec = column * value
+open Types
 
 type 'a node = {mutable prev: 'a node option; mutable next: 'a node option;
                 value: 'a}
 
 type 'a dlist = {mutable first: 'a node option; mutable last: 'a node option}
 
-type t = (string * value Map.t) list
+type t = (string * value Maps.t) list
 
 let lookup = failwith "unimplemented"
 
