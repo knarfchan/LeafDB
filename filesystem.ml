2ea329baa4f06b04484321632d4d3e306ae6090e
An Tran <at697@cornell.edu>
2015-12-03 01:18:00 -0500

    starting filesystem

--- filesystem.ml
+++ filesystem.ml
@@ -1,8 +1,26 @@
 (*filesystem.ml*)
+open Maps
 
 let read_db folder = failwith "not implemented"
 
-let read_tbl file = failwith "not implemented"
+let read_row row acc =
+  let new_row = Array.of_list row in
+    for i = 0 to (Array.length new_row) - 1 do
+      if i = 0 then match new_row.(i) with
+      | "VInt" -> Maps.create (VInt 0)
+      | "VString" -> Maps.create (VString "")
+      | "VBool" -> Maps.create (VBool false)
+      | "VFloat" -> Maps.create (VFloat 0.0)
+      | _ -> failwith "Error: Not a valid SQL type"
+
+let read_tbl_helper file =
+  let matrix = ______ (string list list) in
+  match matrix with
+  | [] ->
+  | h::t ->
+
+let read_tbl file =
+
 
 let add_db db = failwith "not implemented"
 
