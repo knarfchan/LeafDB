8aed7743a553edede469a4bbbd0d97fbfa2eaa09
An Tran <at697@cornell.edu>
2015-12-05 02:51:17 -0500

    table commented

--- table.ml
+++ table.ml
@@ -2,16 +2,17 @@
 open Typs
 open Maps
 open Assertions
+open Unix
 
 type t = (column * Maps.t) list
 
-(* [next_val ()] is a mutable counter used to assign unique rowid's to
- * table rows*)
+(* [next_val ()] is a row id generator *)
 let next_val =
- let counter = ref 0 in fun () ->
-   incr counter;
-   !counter
+  let time = ref (int_of_float (Unix.gettimeofday ())) in fun () ->
+  incr time;
+  !time
 
+(* [matrix_of_table tbl] returns a string matrix of the table *)
 let rec matrix_of_table (tbl:t) =
   match tbl with
   |[] -> []
@@ -22,6 +23,8 @@ let make_select tbl col =
   if List.mem_assoc col tbl then Maps.empty (List.assoc col tbl)
   else raise (Failure "Column specified is not found in table")
 
+(* [select_col tbl rows col acc] accumulates all the values from a table tbl
+ * and column col with a row id in rows *)
 let rec select_col tbl rows col acc =
   match rows with
   | [] -> acc
@@ -32,49 +35,55 @@ let rec select_col tbl rows col acc =
               else select_col tbl t col acc
             else raise (Failure "Column specified is not found in table")
 
+(* [all_col tbl clst rows acc] selects all the values in clst with a row id
+ * in rows *)
 let rec all_col tbl clst rows acc =
   match clst with
   | [] -> acc
-  | h::t -> all_col tbl t rows (acc @ [(h, (select_col tbl rows h (make_select tbl h)))])
+  | h::t -> all_col tbl t rows
+            (acc @ [(h, (select_col tbl rows h (make_select tbl h)))])
 
-(* precondition:
- * postcondition: strip away cols*)
+(* [strip_tbl tbl acc] returns a list of maps in Table tbl*)
 let rec strip_tbl tbl acc : Maps.t list =
   match tbl with
   | [] -> acc
   | (_,b)::t -> (strip_tbl t (acc @ [b]))
 
-let get_size (tbl:t) : int = match tbl with
-  | (a,b)::t -> Maps.size (Maps.get_longest (strip_tbl tbl []) 0 (List.assoc a tbl))
+(* [get_size tbl] returns the size of the table tbl*)
+let get_size (tbl:t) : int =
+  match tbl with
+  | (a,b)::t ->
+      Maps.size (Maps.get_longest (strip_tbl tbl []) 0 (List.assoc a tbl))
   | [] -> 0
 
+(* [get_diff t1 t2] returns the difference in size between t1 and t2*)
 let get_diff (t1:t) (t2:t) : int =
   let l1 = (get_size t1) in
   let l2 = (get_size t2) in
     abs (l1 - l2)
 
-(* precondition:
- * postcondition: *)
+(* [select clst tbl w] selects the columns in clst from table tbl with the
+ * condition w *)
 let select clst tbl w =
   let map =
     (match w with
-    | Condition (col,op,v) -> if List.mem_assoc col tbl then Maps.select (List.assoc col tbl) op v
-                              else raise (Failure "Column specified is not found in table")
-    | Null -> (match clst with
-              | [] -> raise (Failure "No columns specified while selecting")
-              | h::t -> Maps.get_longest (strip_tbl tbl []) 0 (List.assoc h tbl))) in
+    | Condition (col,op,v) ->
+        if List.mem_assoc col tbl then Maps.select (List.assoc col tbl) op v
+        else raise (Failure "Column specified is not found in table")
+    | Null ->
+      (match clst with
+      | [] -> raise (Failure "No columns specified while selecting")
+      | h::t -> Maps.get_longest (strip_tbl tbl []) 0 (List.assoc h tbl))) in
   let rows = Maps.get_rows map in
     (all_col tbl clst rows [])
 
-(* precondition:
- * postcondition: *)
+(* [strip_col tbl acc] returns the list of all the columns in table tbl *)
 let rec strip_col (tbl:t) (acc:column list) :column list =
   match tbl with
   | [] -> acc
   | (a,_)::t -> (strip_col t (acc @ [a]))
 
-(* precondition:
- * postcondition: *)
+(* [selectAll tbl w] selects all columns from table tbl with the condition w*)
 let selectAll tbl w =
   select (List.rev (strip_col tbl [])) tbl w
 
@@ -88,7 +97,8 @@ let rec get_cvlst (clst: column list) (vlst: value list)
   | h::t, h'::t' -> get_cvlst t t' (acc @ [(h,h')])
   | _, _ -> raise (Failure "List of columns and values must be the same length")
 
-(* [insert_help tbl cvlst rowid acc] *)
+(* [insert_help tbl cvlst rowid acc] inserts the values from cvlst into tbl with
+ * the row id rowid*)
 let rec insert_help (tbl:t) (cvlst: (column * value) list) rowid acc =
   match tbl with
   | [] -> acc
@@ -98,65 +108,70 @@ let rec insert_help (tbl:t) (cvlst: (column * value) list) rowid acc =
         (acc @ [(name, Maps.insert (List.assoc name cvlst) rowid map)])
       else insert_help tl cvlst rowid (acc @ [name, map])
 
-
+(* [insert tbl clst vlst] inserts the item specified by clst and vlst into tbl*)
 let insert (tbl:t) (clst:column list) (vlst: value list) : t =
   let cvlst = (get_cvlst clst vlst []) in
   let rowid = next_val () in
     insert_help tbl cvlst rowid []
 
-
+(* [insertAll_help tbl vlst rowid acc] inserts the values from vlst into tbl
+ *  with the row id rowid *)
 let rec insertAll_help (tbl:t) (vlst: value list) rowid acc =
   match tbl, vlst with
     | (name, map)::tl, a::b ->
         (insertAll_help tl b rowid (acc @ [(name, Maps.insert a rowid map)]))
     | _, _ -> acc
 
+(* [insertAll] inserts the values in vlst into tbl *)
 let insertAll (tbl:t) (vlst: value list) : t =
   let rowid = next_val () in
     insertAll_help tbl vlst rowid []
 
-(* precondition:
- * postcondition: *)
+(* [get_col cvlst acc] returns the columns of a cvlst *)
 let rec get_col cvlst acc =
   match cvlst with
   | [] -> acc
   | (c,_)::t -> get_col t (acc @ [c])
 
+(* [get_val_from_cvlst cvlst acc] returns the values from a cvlst *)
 let rec get_val_from_cvlst cvlst acc =
   match cvlst with
   | [] -> acc
   | (_,v)::t -> get_val_from_cvlst t (acc @ [v])
 
-
-
+(* [get_all_rows table where] returns the rows of all the items in table that
+ * satisfies the where condition *)
 let rec get_all_rows table where =
   match table, where with
-  | ((name,map)::t), (Condition (col,op,v)) -> (if (name = col) then Maps.select map op v
-                                                                else get_all_rows t where)
+  | ((name,map)::t), (Condition (col,op,v)) ->
+      (if (name = col) then Maps.select map op v else get_all_rows t where)
   | ((name,map)::t) , Null -> map
   | _ -> raise (Failure "Column specified is not found in table")
 
+(* [make_removed ids table] remove the items from table with the row id ids*)
 let rec make_removed ids table =
   match table with
   | (name,map)::t -> (name, (Maps.delete ids map))::(make_removed ids t)
   | [] -> []
 
+(* [delete table where] deletes items from a table that satisfies the where
+ * condition *)
 let delete table where =
   let rows = get_all_rows table where in
   let ids = Maps.get_rows rows in
   make_removed ids table
 
+(* [create_help cdl acc] creates a table with the columns specified in cdl*)
 let rec create_help cdl acc =
   match cdl with
   | [] -> acc
   | (col, v)::t -> create_help t ((col, Maps.create v)::acc)
 
-(* [create cdl] creates an empty table
- * precondition:
- * postcondition:  *)
+(* [create cdl] creates an empty table *)
 let rec create (cdl : column_dec list) =
   create_help cdl []
 
+(* [get_row row tbl acc] gets all the items in one row in table tbl *)
 let rec get_row row tbl acc =
   match tbl with
   | [] -> acc
@@ -164,22 +179,27 @@ let rec get_row row tbl acc =
                         get_row row t (acc @ [(Maps.lookup row map)])
                       else get_row row t (acc @ [VNull])
 
+(* [get_all_rows row tbl acc] gets all the rows in the table rows from tbl *)
 let rec get_all_rows rows tbl acc =
   match rows with
   | [] -> acc
   | h::t -> get_all_rows t tbl acc @ [(get_row h tbl [])]
 
+(* [tbl_to_matrix tbl acc] converts a table to a matrix and accumulates
+ * into acc *)
 let rec tbl_to_matrix (tbl : t) acc =
   match tbl with
   | [] -> [[]]
-  | (a,b)::t -> let new_map = Maps.get_longest (strip_tbl tbl []) 0 (List.assoc a tbl) in
-              let rows = Maps.get_rows new_map in
-                get_all_rows rows tbl acc
+  | (a,b)::t ->
+    let new_map = Maps.get_longest (strip_tbl tbl []) 0 (List.assoc a tbl) in
+      let rows = Maps.get_rows new_map in
+        get_all_rows rows tbl acc
 
+(* [convert_matrix] converts a table to a matrix *)
 let convert_matrix (tbl:t) =
   tbl_to_matrix tbl []
 
-
+(* [row_val r acc] converts the value in a row to a string *)
 let rec row_val (r:value list) acc =
   match r with
   | [] -> acc
@@ -189,16 +209,19 @@ let rec row_val (r:value list) acc =
   | VFloat x::t -> row_val t (acc @ [string_of_float x])
   | VNull::t -> row_val t (acc @ ["NULL"])
 
+(* [tbl_val m acc] converts a value list list to a string list list*)
 let rec tbl_val (m: value list list) acc: string list list=
   match m with
   | [] -> acc
   | h::t -> tbl_val t (acc @ [row_val h []])
 
+(* [row_to_array] converts all the rows of a matrix to arrays *)
 let rec row_to_array (m: column list list) acc =
   match m with
   | [] -> acc
   | h::t -> row_to_array t (acc @ [Array.of_list h])
 
+(* [row_size tbl] returns a list of a the longest string in each column *)
 let row_size tbl =
   let size = Array.make (Array.length tbl.(0)) 0 in
     (for i = 0 to (Array.length tbl) - 1 do
@@ -210,20 +233,24 @@ let row_size tbl =
     done);
   size
 
-(* precondition: the number of columns of tbl and size must be the same
- * postcondition: format_array returns a string matrix where all values are the same length*)
+(* [format_array tbl size] returns a string matrix where all values are the
+ * same length*)
 let format_array tbl size =
   (for i = 0 to (Array.length tbl) - 1 do
     for j = 0 to (Array.length tbl.(0)) -1 do
-      tbl.(i).(j) <- (Bytes.make (size.(j) - (Bytes.length tbl.(i).(j))) ' ') ^ (tbl.(i).(j))
+      tbl.(i).(j) <-
+      (Bytes.make (size.(j) - (Bytes.length tbl.(i).(j))) ' ') ^ (tbl.(i).(j))
     done
   done); tbl
 
+(* [get_bars size] is used to generate the column bars in printing *)
 let get_bars (size: int array) length:int=
   (for i = 0 to (Array.length size) - 1 do
     length := !length + size.(i)
   done); length := !length + ((Array.length size) * 3) + 1; !length
 
+(* [print_tbl_helper tbl] is used to help print a table from a string array
+ * array *)
 let print_tbl_helper (tbl:string array array) =
   let total_size = row_size tbl in
   let bar = get_bars total_size (ref 0) in
@@ -238,10 +265,13 @@ let print_tbl_helper (tbl:string array array) =
         else (Printf.printf "%s\n" " |")
     done); (Printf.printf "%s\n" (" " ^ (Bytes.make bar '-')))
 
+(* [print_tbl tbl] prints the table tbl *)
 let print_tbl (tbl:t) =
-  Printf.printf "\n"; print_tbl_helper (Array.of_list (row_to_array ((strip_col tbl []) ::
+  Printf.printf "\n"; print_tbl_helper
+  (Array.of_list (row_to_array ((strip_col tbl []) ::
   (tbl_val (convert_matrix tbl) [])) []))
 
+(* [get_val tbl row acc] gets all the values from a row in a table *)
 let rec get_vals (tbl:t) row acc =
   match tbl with
   | [] -> acc
@@ -249,62 +279,78 @@ let rec get_vals (tbl:t) row acc =
                         get_vals t row (acc @ [(name, Maps.lookup row map)])
                       else get_vals t row acc
 
+(* [get_cvlst t1 t2 rows acc] returns a cvlst of the values from 2 different
+ * tables *)
 let rec get_cvlst (t1:t) (t2:t) rows acc =
   match rows with
   | [] -> acc
-  | (r1, r2)::t -> get_cvlst t1 t2 t (acc @ [(get_vals t1 r1 []) @ (get_vals t2 r2 [])])
+  | (r1, r2)::t -> get_cvlst t1 t2 t
+                   (acc @ [(get_vals t1 r1 []) @ (get_vals t2 r2 [])])
 
+(* [empty_table tbl acc] returns an empty table *)
 let rec empty_table tbl acc =
   match tbl with
   | [] -> acc
   | (name, map)::t -> (empty_table t (acc @ [(name, Maps.empty map)]))
 
+(* [join_help tbl cvlst] creates a new table based on the cvlst *)
 let rec join_help tbl cvlst =
   match cvlst with
   | [] -> tbl
-  | lst::t -> join_help (insert tbl (get_col lst []) (get_val_from_cvlst lst [])) t
+  | lst::t -> join_help (insert tbl (get_col lst [])
+              (get_val_from_cvlst lst [])) t
 
+(* [remove_ontbl col acc] removes one column specified in the on command of a
+ * join *)
 let rec remove_on tbl col acc =
   match tbl with
   | [] -> acc
-  | (name, map)::t -> if col = name then acc @ t else remove_on t col (acc @ [(name, map)])
+  | (name, map)::t -> if col = name then acc @ t
+                      else remove_on t col (acc @ [(name, map)])
 
-(* precondition:
- * postcondition: *)
+(* [join t1 t2 o] joins two tables t1 and t2 accordingly to the on command *)
 let join t1 t2 o =
-  let rows = (match o with
-             | (c1, c2) -> if List.mem_assoc c1 t1 && List.mem_assoc c2 t2 then
-                             Maps.join (List.assoc c1 t1) (List.assoc c2 t2)
-                           else raise (Failure "Column specified is not found in table")) in
-    List.rev (remove_on (List.rev (join_help ((empty_table t1 []) @
+  let rows =
+    (match o with
+    | (c1, c2) -> if List.mem_assoc c1 t1 && List.mem_assoc c2 t2 then
+                    Maps.join (List.assoc c1 t1) (List.assoc c2 t2)
+                  else raise (Failure "Column specified is not found in table"))
+    in List.rev (remove_on (List.rev (join_help ((empty_table t1 []) @
       (empty_table t2 [])) (get_cvlst t1 t2 rows []))) (snd o) [])
 
-
-(*I am sad*)
+(* [get_maps rowids tbl name new_map acc] returns a column using the rowids and
+ * accumulates the new items inserted *)
 let rec get_maps rowids tbl name new_map acc =
   match rowids with
   | [] -> (new_map, acc)
-  | h::t -> if Maps.is_member h (List.assoc name tbl) then
-              (get_maps t tbl name (Maps.insert (Maps.lookup h (List.assoc name tbl)) h new_map) acc)
-            else
-              (get_maps t tbl name (Maps.insert (Maps.get_type new_map) h new_map)
-              (acc @ [(name, h, Maps.get_type new_map)]))
+  | h::t ->
+    if Maps.is_member h (List.assoc name tbl) then
+      (get_maps t tbl name (Maps.insert
+      (Maps.lookup h (List.assoc name tbl)) h new_map) acc)
+    else
+      (get_maps t tbl name (Maps.insert (Maps.get_type new_map) h new_map)
+      (acc @ [(name, h, Maps.get_type new_map)]))
 
-let rec get_all_maps rowidlst tbl (maps:t) (acc:t) inserts =
+(* [get_all_maps] returns a table with the row ids in the list rowids and
+ * returns a list of the items inserted*)
+let rec get_all_maps rowid tbl (maps:t) (acc:t) inserts =
   match maps with
   | [] -> (acc, inserts)
   | (name, map)::t ->
-    let nmap = (get_maps rowidlst tbl name (Maps.create (Maps.get_type map)) []) in
-    get_all_maps rowidlst tbl t (acc @ [(name, fst nmap)]) (inserts @ (snd nmap))
+    let nmap = (get_maps rowid tbl name (Maps.create (Maps.get_type map)) []) in
+    get_all_maps rowid tbl t (acc @ [(name, fst nmap)]) (inserts @ (snd nmap))
 
-(* precondition:
- * postcondition: *)
+(* [update_help new_maps cvlst acc] updates the new maps with the values from
+ * cvlst *)
 let rec update_help new_maps cvlst acc =
   match new_maps, cvlst with
   | [], [] -> acc
   | (a,b)::t, (c,v)::t' -> update_help t t' (acc @ [(a, Maps.update b v)])
-  | _, _ -> raise (Failure "Different number of columns in table and column list")
+  | _, _ ->
+    raise (Failure "Different number of columns in table and column list")
 
+(* [update_all_col tbl new_tbl acc] updates all the values in the original table
+ * with the values in the new table*)
 let rec update_all_col tbl new_tbl acc =
   match tbl with
   | [] -> acc
@@ -314,29 +360,34 @@ let rec update_all_col tbl new_tbl acc =
                         then Maps.replace map (List.assoc name new_tbl)
                        else map))])
 
+(* [insert_nulls tbl inserts acc] inserts once null values into a table *)
 let rec insert_nulls tbl inserts (acc:t) =
   match inserts with
   | [] -> acc
-  | (name, rowid, v)::t -> insert_nulls tbl t (acc @ [(name, Maps.insert v rowid (List.assoc name tbl))])
+  | (name, rowid, v)::t ->  insert_nulls tbl t (acc @
+                            [(name, Maps.insert v rowid (List.assoc name tbl))])
 
+(* [insert_all_nulls] inserts the updated null values back into tbl *)
 let rec insert_all_nulls (tbl:t) null_cols (acc:t):t =
   match tbl with
   | [] -> acc
-  | (name, map)::t -> if List.mem_assoc name null_cols then
-                        insert_all_nulls t null_cols (acc @ [(name, List.assoc name null_cols)])
-                      else insert_all_nulls t null_cols (acc @ [name, map])
+  | (name, map)::t ->
+    if List.mem_assoc name null_cols then
+      insert_all_nulls t null_cols (acc @ [(name, List.assoc name null_cols)])
+    else insert_all_nulls t null_cols (acc @ [name, map])
 
-(* precondition:
- * postcondition: *)
+(* [update tbl cvlst w] updates the items specified by where with the values
+ * from cvlst into tbl*)
 let update tbl cvlst w =
   let new_tbl = select (get_col cvlst []) tbl w in
-  let rowids = Maps.get_rows (Maps.get_longest (strip_tbl new_tbl []) 0 (Maps.create (VInt 0))) in
-  let new_maps = (get_all_maps rowids tbl new_tbl [] []) in
-  let updated_tbl = (update_help (fst new_maps) cvlst []) in
-    ((print_tbl new_tbl); (print_tbl updated_tbl);
-    (update_all_col (insert_all_nulls tbl (insert_nulls tbl (snd new_maps) []) []) updated_tbl []))
+    let rowids = Maps.get_rows (Maps.get_longest (strip_tbl new_tbl []) 0
+                 (Maps.create (VInt 0))) in
+    let new_maps = (get_all_maps rowids tbl new_tbl [] []) in
+    let updated_tbl = (update_help (fst new_maps) cvlst []) in
+      (update_all_col (insert_all_nulls tbl (insert_nulls tbl (snd new_maps) [])
+      []) updated_tbl [])
 
-(*
+
 TEST_MODULE "insert_test" = struct
 
   let tbl = [("Name", Maps.create (VString "")); ("Age", Maps.create (VInt 0));
@@ -404,7 +455,7 @@ TEST_MODULE "insert_test" = struct
 
   let _ = print_tbl dj''*)
 
-end*)
+end
 (*
 TEST_MODULE "insert_test" = struct
 
