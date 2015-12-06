82dad4309a801849032c07c95b69bd58b799f23a
Frank Chan <fc249@cornell.edu>
2015-12-05 19:28:36 -0500

    document dbms and add change text to string

--- dbms.ml
+++ dbms.ml
@@ -5,14 +5,10 @@ type t = (string, Database.t) Hashtbl.t
 let create () : t =
   Hashtbl.create 5
 
-(* adds an empty database to a DBMS *)
 let add_database (dbs: t) (str: string) : bool =
   if Hashtbl.mem dbs str then false
   else (Hashtbl.add dbs str (Database.create()); true)
 
-(* [use dbs str] takes the name of a database and returns it
- * precondition  : none
- * postcondition : *)
 let use (dbs: t) (str: string) : Database.t option =
   if Hashtbl.mem dbs str then Some (Hashtbl.find dbs str)
   else None
