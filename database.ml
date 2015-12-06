1b09ad7b38fa51c44bbdb3eb939af14f5f914526
Frank Chan <fc249@cornell.edu>
2015-12-05 20:13:40 -0500

    document database

--- database.ml
+++ database.ml
@@ -8,19 +8,13 @@ type t = (string, Table.t) Hashtbl.t
 let create () : t =
   Hashtbl.create 7
 
-(* [add_table db str tbl] adds a table to a specified database
- * precondition  : none
- * postcondition : mem tables str ~ add_table db str tbl
-                   mem tables str ~ add_table db str tbl *)
 let add_table (db: t) (str: string) (tbl: Table.t) : bool =
   if Hashtbl.mem db str then false
   else ((Hashtbl.add db str tbl); true)
 
-(* specs will go here *)
 let update_table (db: t) (str: string) (tbl: Table.t) : unit =
   Hashtbl.replace db str tbl
 
-(* takes the table with name str, removes it from the database *)
 let drop (db: t) (str: string) : bool =
   if Hashtbl.mem db str then (Hashtbl.remove db str; true)
   else false
